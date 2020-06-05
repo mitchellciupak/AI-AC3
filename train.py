4bb30570eefbfc9a8d5a7e13db9213fced03f436
mitchellciupak <ciupak@purdue.edu>
2020-06-05 11:34:53 -0500

    ensure shared gradients init, done

--- train.py
+++ train.py
@@ -11,3 +11,10 @@ import torch.nn.functional as F
 from envs import create_atari_env
 from model import ActorCritic
 from torch.autograd import Variable
+
+# Init ensurance the models share the same gradient
+def ensure_shared_grads(model, shared_model):
+    for param, shared_param in zip(model.parameters(), shared_model.parameters()):
+        if shared_param.grad is not None:
+            return
+        shared_param._grad = param.grad
