64ce502503d495ca131a2e1e38c8af7451fcb9e2
mitchellciupak <ciupak@purdue.edu>
2020-06-05 10:49:05 -0500

    model init, import

---
+++ model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 10:47:22 2020
+
+@author: mjcre
+"""
+
+#Import
+import numpy as np
+import torch
+import tourch.nn as nn
+import tourch.nn.functional as F
