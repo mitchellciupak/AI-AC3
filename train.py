c7ea12d8d718757f6817aa40e9abe17886d93cd6
mitchellciupak <ciupak@purdue.edu>
2020-06-05 11:38:27 -0500

    the start of a long ass training function

--- train.py
+++ train.py
@@ -18,3 +18,14 @@ def ensure_shared_grads(model, shared_model):
         if shared_param.grad is not None:
             return
         shared_param._grad = param.grad
+        
+        
+def train(rank, params, shared_model, optimizer):
+    torch.manual_seed(params.seed + rank) # shifting the seed with rank to asynchronize each training agent
+    env = create_atari_env(params.env_name) # creating an optimized environment thanks to the create_atari_env function
+    env.seed(params.seed + rank) # aligning the seed of the environment on the seed of the agent
+    model = ActorCritic(env.observation_space.shape[0], env.action_space) # creating the model from the ActorCritic class
+    state = env.reset() # state is a numpy array of size 1*42*42, in black & white
+    state = torch.from_numpy(state) # converting the numpy array into a torch tensor
+    done = True # when the game is done
+    episode_length = 0 # initializing the length of an episode to 0
